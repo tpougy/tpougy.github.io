9167b6ba59b902f32af3cd10d387539702eff36a
tpougy <thomazpougy@gmail.com>
2026-01-20 23:54:37 -0300

    astro update

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,6 @@ import { pluginCodeOutput } from "@fujocoded/expressive-code-output";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
-import { vitePluginSvelteH2J } from "@ethercorps/svelte-h2j/vite"; // Re-importa o plugin
 
 const isDev = process.env.NODE_ENV === "development";
 
@@ -40,14 +39,11 @@ export default defineConfig({
     ...(isDev ? [keystatic()] : []), // uses the integration conditionally
   ],
   vite: {
-    plugins: [
-      vitePluginSvelteH2J(), // Re-adiciona o plugin svelte-h2j
-    ],
     build: {
       rollupOptions: {
         external: ["/pagefind/pagefind.js?url"],
       },
     },
   },
-  output: isDev ? "hybrid" : "static", // only set hybrid rendering for dev mode
+  output: "static",
 });
